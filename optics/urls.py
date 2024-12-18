f689c10b492e886914c94526f85c98983d60c41c
dan.toropov <dan.toropov@gmail.com>
2024-12-18 21:58:43 +0300

    create product page

--- optics/urls.py
+++ optics/urls.py
@@ -1,7 +1,8 @@
 from optics.apps import OpticsConfig
 from django.urls import path
 
-from optics.views import mainpage, FeedbackCreateView, thank_you, about
+from optics.views import mainpage, FeedbackCreateView, thank_you, about, \
+    product_list_view, ServiceListView
 
 app_name = OpticsConfig.name
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('contact/', FeedbackCreateView.as_view(), name='contact'),
     path('thank-you/', thank_you, name='thank-you'),
     path('about/', about, name='about'),
-
+    path('shop-list/', product_list_view, name='shop-list'),
+    path('service-list/', ServiceListView.as_view(), name='service-list'),
 ]
--- optics/views.py
+++ optics/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from config.settings import NUMBER_OF_PRODUCTS_DISPLAYED
 from optics.models import Category, Product, Feedback, Service
@@ -37,6 +37,37 @@ class FeedbackCreateView(CreateView):
     fields = ['name', 'phone', 'message']
     template_name = 'optics/contact.html'
     success_url = reverse_lazy('optics:thank-you')
+
+def product_list_view(request):
+
+    category_list = Category.objects.filter(is_published=True)
+
+    object_list = []
+    for cat in category_list:
+        product_list = Product.objects.filter(is_published=True, category=cat).order_by('name')
+        obj = {'category': cat, 'product_list': product_list}
+        object_list.append(obj)
+
+    context = {
+        'object_list' : object_list,
+        'category_list' : category_list,
+        'testimonials' : get_random_reviews(),
+        'quote' : get_random_quote()
+    }
+
+    return render(request, 'optics/shop_list.html', context)
+
+
+class ServiceListView(ListView):
+    model = Product
+    template_name = 'optics/shop_list.html'
+    # paginate_by = 3
+    extra_context = {
+    }
+    ordering = ['name']
+
+    def get_queryset(self):
+        return Product.objects.filter(is_published=True)
 
 def thank_you(request):
     context = {
