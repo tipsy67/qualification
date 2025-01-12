0191d80018a3ec9ab2475e4f2977f810e8efb851
dan.toropov <dan.toropov@gmail.com>
2025-01-12 23:00:44 +0300

    fix  cart detail

--- cart/views.py
+++ cart/views.py
@@ -11,7 +11,14 @@ def cart_add(request, product_id):
     if request.user.is_authenticated:
         cart = Cart(request)
         product = get_object_or_404(Product, id=product_id)
-        cart.add(product=product, quantity=1, )
+        if request.method == 'POST':
+            form = CartAddProductForm(request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                cart.add(product=product, quantity = cd['quantity'], override_quantity = cd['override'])
+        else:
+            cart.add(product=product, quantity=1, )
+
         url = request.META.get('HTTP_REFERER') + "#product_" +str(product_id)
         return redirect(url)
     else:
